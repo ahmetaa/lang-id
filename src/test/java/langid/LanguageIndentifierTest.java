b64ef6d1d4884331c46f9cad978a632209aa5c32
ahmetaa <ahmetaa@gmail.com>
2017-01-14 21:54:28 +0300

    better tests

--- src/test/java/langid/LanguageIndentifierTest.java
+++ src/test/java/langid/LanguageIndentifierTest.java
@@ -7,19 +7,40 @@ import java.io.IOException;
 
 public class LanguageIndentifierTest {
     @Test
-    public void modelGroupTest() throws IOException {
+    public void allModelTest() throws IOException {
         LanguageIdentifier lid = LanguageIdentifier.fromInternalModels();
         Assert.assertEquals("tr", lid.identify("merhaba dünya ve tüm gezegenler"));
         Assert.assertEquals("en", lid.identify("hello world and all the planets what is this?"));
+        Assert.assertEquals("es", lid.identify("Hola mundo y todos los planetas"));
         Assert.assertEquals("fr", lid.identify("Bonjour tout le monde et toutes les planètes"));
         Assert.assertEquals("az", lid.identify("Salam dünya və bütün planetlərin bu həqiqətən pis olur"));
     }
 
     @Test
-    public void allModelTest() throws IOException {
+    public void allModelSamplingTest() throws IOException {
         LanguageIdentifier lid = LanguageIdentifier.fromInternalModels();
+        Assert.assertEquals("tr", lid.identify("merhaba dünya ve tüm gezegenler", 20));
+        Assert.assertEquals("en", lid.identify("hello world and all the planets what is this?", 20));
+        Assert.assertEquals("es", lid.identify("Hola mundo y todos los planetas", 20));
+        Assert.assertEquals("fr", lid.identify("Bonjour tout le monde et toutes les planètes", 20));
+        Assert.assertEquals("az", lid.identify("Salam dünya və bütün planetlərin bu həqiqətən pis olur", 20));
+    }
+
+    @Test
+    public void allModelFastSamplingTest() throws IOException {
+        LanguageIdentifier lid = LanguageIdentifier.fromInternalModels();
+        Assert.assertEquals("tr", lid.identifyFast("merhaba dünya ve tüm gezegenler", 20));
+        Assert.assertEquals("en", lid.identifyFast("hello world and all the planets what is this?", 20));
+        Assert.assertEquals("es", lid.identifyFast("Hola mundo y todos los planetas", 20));
+        Assert.assertEquals("fr", lid.identifyFast("Bonjour tout le monde et toutes les planètes", 20));
+        Assert.assertEquals("az", lid.identifyFast("Salam dünya və bütün planetlərin bu həqiqətən pis olur", 20));
+    }
+
+    @Test
+    public void modelGroupTest() throws IOException {
+        LanguageIdentifier lid = LanguageIdentifier.fromInternalModelGroup("tr_group");
         Assert.assertEquals("tr", lid.identify("merhaba dünya ve tüm gezegenler"));
-        Assert.assertEquals("es", lid.identify("Hola mundo y todos los planetas"));
+        Assert.assertEquals("unk", lid.identify("Hola mundo y todos los planetas"));
     }
 
     @Test
